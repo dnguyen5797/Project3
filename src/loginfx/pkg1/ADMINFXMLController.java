8be0ca90e246afccb3f215a3ab910ff244d6678e
jarre <jarre@10.213.178.209>
2017-11-20 13:17:11 -0500

    Initial Commit

---
+++ src/loginfx/pkg1/ADMINFXMLController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginfx.pkg1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Justin
+ */
+public class ADMINFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    
+    @FXML
+    private Button btnLogout;
+    
+    
+    public void Logout(ActionEvent event) throws IOException{
+        Parent youInParent = FXMLLoader.load(getClass().getResource("LoginFXML.fxml"));
+            Scene youInScene = new Scene(youInParent);
+            //this line gets the stage information
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            
+            window.setScene(youInScene);
+            window.show();
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
