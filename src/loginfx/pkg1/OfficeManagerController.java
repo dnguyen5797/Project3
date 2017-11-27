59dfa78474f9fa13a6abd95ecbb42defcd15e615
jarre <jarre@10.213.178.209>
2017-11-27 12:56:50 -0500

    Added OfficeManager FXML and started editing the file for the Office Manager's functions. Also deleted the unnecessary OMFXML Files

--- src/loginfx/pkg1/OfficeManagerController.java
+++ src/loginfx/pkg1/OfficeManagerController.java
@@ -7,7 +7,12 @@ package loginfx.pkg1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +21,33 @@ import javafx.fxml.Initializable;
  */
 public class OfficeManagerController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextArea ExaminePartsTextArea;
+    
+    @FXML
+    private Button SearchPartNameButton;
+    
+    @FXML
+    private Button SearchPartNumberButton;
+    
+    @FXML
+    private TextField SearchPartNameText;
+    
+    @FXML
+    private TextField SearchPartNumberText;
+    
+    private void SearchByPartNameAction(ActionEvent SearchByPartName){
+        
+    }
+    private void SearchByPartNumberAction(ActionEvent SearchByPartNumber){
+        
+    }
+    private void LogoutButtonAction(ActionEvent LogoutButtonAction){
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        System.out.println("This happens first.");
     }    
     
 }
