9fc86699553391279be4b486e6260623b0829d96
Duy <Duy@10.213.179.210>
2017-11-27 22:28:07 -0500

    test

---
+++ src/loginfx/pkg1/SALESFXMLController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginfx.pkg1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Justin
+ */
+public class SALESFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private Button btnLogout;
+    @FXML
+    private Button btnButton;
+    @FXML
+    private TextArea txtArea;
+    
+    public void Logout(ActionEvent event) throws IOException{
+        Parent youInParent = FXMLLoader.load(getClass().getResource("LoginFXML.fxml"));
+            Scene youInScene = new Scene(youInParent);
+            //this line gets the stage information
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            
+            window.setScene(youInScene);
+            window.show();
+    }
+    
+    
+    public void Show(ActionEvent event){
+        txtArea.setText("(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻\n(╯°□°）╯︵ ┻━┻");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
